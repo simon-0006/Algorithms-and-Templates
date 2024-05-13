9760d746184468a3001a06e055e67ad20d43dff2
Simon Ganter <gantersim@gmail.com>
2024-05-13 19:42:50 +0200

    template added

---
+++ template.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <unordered_set>
+
+using namespace std;
+
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+
+void sol(int a, int b) {
+    cout << "Solution" << endl;
+}
+
+
+int main() {
+    int t; cin >> t;
+    int a,b;
+    while (t--) {
+        cin >> a >> b;
+        sol(a, b);
+    }
+    return 0;
+}
