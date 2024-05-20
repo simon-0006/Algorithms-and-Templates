d841b0143e31b0a52090a85ad33e9d905ccc9ddc
Simon Ganter <gantersim@gmail.com>
2024-05-20 16:29:50 +0200

    MaxFlow some changes

--- Graphs/MaxFlow.cpp
+++ Graphs/MaxFlow.cpp
@@ -102,11 +102,11 @@ int main() {
         }
     }
 
-    graph[0][0] =  0; graph[0][1] = 16; graph[0][2] = 13; graph[0][3] =  0; graph[0][4] =  0; graph[0][5] =  0;
-    graph[1][0] =  0; graph[1][1] =  0; graph[1][2] = 10; graph[1][3] = 12; graph[1][4] =  0; graph[1][5] =  0;
-    graph[2][0] =  0; graph[2][1] =  4; graph[2][2] =  0; graph[2][3] =  0; graph[2][4] = 14; graph[2][5] =  0;
-    graph[3][0] =  0; graph[3][1] =  0; graph[3][2] =  9; graph[3][3] =  0; graph[3][4] =  0; graph[3][5] = 20;
-    graph[4][0] =  0; graph[4][1] =  0; graph[4][2] =  0; graph[4][3] =  7; graph[4][4] =  0; graph[4][5] =  4;
+    graph[0][0] =  0; graph[0][1] =  5; graph[0][2] =  7; graph[0][3] =  0; graph[0][4] =  0; graph[0][5] =  0;
+    graph[1][0] =  0; graph[1][1] =  0; graph[1][2] =  7; graph[1][3] =  4; graph[1][4] =  0; graph[1][5] =  0;
+    graph[2][0] =  0; graph[2][1] =  0; graph[2][2] =  0; graph[2][3] =  3; graph[2][4] =  3; graph[2][5] =  0;
+    graph[3][0] =  0; graph[3][1] =  0; graph[3][2] =  0; graph[3][3] =  0; graph[3][4] =  4; graph[3][5] =  5;
+    graph[4][0] =  0; graph[4][1] =  0; graph[4][2] =  0; graph[4][3] =  0; graph[4][4] =  0; graph[4][5] =  6;
     graph[5][0] =  0; graph[5][1] =  0; graph[5][2] =  0; graph[5][3] =  0; graph[5][4] =  0; graph[5][5] =  0;
 
     MaxFlow mf;
